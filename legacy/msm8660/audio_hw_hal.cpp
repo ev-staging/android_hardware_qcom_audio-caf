LineageOS/android_hardware_qcom_audio

Use standard initializers
Change-Id: I7582d391cb3b9f98bba06fb15e7a6798930749e9

legacy/alsa_sound/audio_hw_hal.cpp
@@ -991,21 +991,21 @@ err_create_audio_hw:
 }
 
 static struct hw_module_methods_t qcom_audio_module_methods = {
-        open: qcom_adev_open
+        .open = qcom_adev_open
 };
 
 struct qcom_audio_module HAL_MODULE_INFO_SYM = {
-    module: {
-        common: {
-            tag: HARDWARE_MODULE_TAG,
-            module_api_version: AUDIO_MODULE_API_VERSION_0_1,
-            hal_api_version: HARDWARE_HAL_API_VERSION,
-            id: AUDIO_HARDWARE_MODULE_ID,
-            name: "QCOM Audio HW HAL",
-            author: "The Linux Foundation",
-            methods: &qcom_audio_module_methods,
-            dso : NULL,
-            reserved : {0},
+    .module = {
+        .common = {
+            .tag = HARDWARE_MODULE_TAG,
+            .module_api_version = AUDIO_MODULE_API_VERSION_0_1,
+            .hal_api_version = HARDWARE_HAL_API_VERSION,
+            .id = AUDIO_HARDWARE_MODULE_ID,
+            .name = "QCOM Audio HW HAL",
+            .author = "The Linux Foundation",
+            .methods = &qcom_audio_module_methods,
+            .dso = NULL,
+            .reserved = {0},
         },
     },
 };

ev-staging/android_hardware_qcom_audio-caf

legacy/msm8660/audio_hw_hal.cpp
@@ -825,21 +825,21 @@ err_create_audio_hw:
 }
 
 static struct hw_module_methods_t qcom_audio_module_methods = {
-        open: qcom_adev_open
+        .open = qcom_adev_open
 };
 
 struct qcom_audio_module HAL_MODULE_INFO_SYM = {
-    module: {
-        common: {
-            tag: HARDWARE_MODULE_TAG,
-            module_api_version:  AUDIO_DEVICE_API_VERSION_1_0,
-            hal_api_version: HARDWARE_HAL_API_VERSION,
-            id: AUDIO_HARDWARE_MODULE_ID,
-            name: "QCOM Audio HW HAL",
-            author: "Code Aurora Forum",
-            methods: &qcom_audio_module_methods,
-            dso : NULL,
-            reserved : {0},
+    .module = {
+        .common = {
+            .tag = HARDWARE_MODULE_TAG,
+            .module_api_version = AUDIO_DEVICE_API_VERSION_1_0,
+            .hal_api_version = HARDWARE_HAL_API_VERSION,
+            .id = AUDIO_HARDWARE_MODULE_ID,
+            .name = "QCOM Audio HW HAL",
+            .author = "Code Aurora Forum",
+            .methods = &qcom_audio_module_methods,
+            .dso = NULL,
+            .reserved = {0},
         },
     },
 };